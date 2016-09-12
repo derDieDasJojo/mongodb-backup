dbf345ea1e6b5d773bba1fc9a96dc57931875205
Jojo <rueckwerts@bitbucket.org>
2016-09-12 15:48:23 +0200

    added possibility to save backups remote with scp+sftp

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ MAINTAINER Tutum Labs <support@tutum.co>
 RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
     echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list && \
     apt-get update && \
-    apt-get install -y mongodb-org-shell mongodb-org-tools && \
+    apt-get install -y mongodb-org-shell mongodb-org-tools sshpass sshfs&& \
     echo "mongodb-org-shell hold" | dpkg --set-selections && \
     echo "mongodb-org-tools hold" | dpkg --set-selections && \
     mkdir /backup
@@ -12,5 +12,5 @@ RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 && \
 ENV CRON_TIME="0 0 * * *"
 
 ADD run.sh /run.sh
-VOLUME ["/backup"]
+#VOLUME ["/backup"]
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -14,6 +14,8 @@ MONGODB_PASS=${MONGODB_PASS:-${MONGODB_ENV_MONGODB_PASS}}
 [[ ( -n "${MONGODB_DB}" ) ]] && USER_STR=" --db ${MONGODB_DB}"
 
 BACKUP_CMD="mongodump --out /backup/"'${BACKUP_NAME}'" --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} ${EXTRA_OPTS}"
+BACKUP_UPLOAD_CMD="sshpass -p '"${BACKUP_SERVER_PASS}"' scp -r -oStrictHostKeyChecking=no /backup/"'${BACKUP_NAME}'" ${BACKUP_SERVER_USER}@${BACKUP_SERVER}:/mongobackup/${BACKUP_REMOTE_SUBFOLDER}/${BACKUP_NAME}"
+SFTP_CMD="sshpass -p ${BACKUP_SERVER_PASS} sftp ${BACKUP_SERVER_USER}@${BACKUP_SERVER}"
 
 echo "=> Creating backup script"
 rm -f /backup.sh
@@ -23,8 +25,14 @@ MAX_BACKUPS=${MAX_BACKUPS}
 BACKUP_NAME=\$(date +\%Y.\%m.\%d.\%H\%M\%S)
 
 echo "=> Backup started"
+#echo $BACKUP_CMD
 if ${BACKUP_CMD} ;then
     echo "   Backup succeeded"
+
+    echo "=> Uploading Backup stardted"
+    echo ${BACKUP_UPLOAD_CMD}
+    ${BACKUP_UPLOAD_CMD} && echo "  Upload succeeded" || echo "  Upload failed"
+
 else
     echo "   Backup failed"
     rm -rf /backup/\${BACKUP_NAME}
@@ -38,6 +46,21 @@ if [ -n "\${MAX_BACKUPS}" ]; then
         rm -rf /backup/\${BACKUP_TO_BE_DELETED}
     done
 fi
+
+if [ -n "\${MAX_REMOTE_BACKUPS}" ]; then
+    while [ \$(echo ls /mongobackup/${BACKUP_REMOTE_SUBFOLDER} | ${SFTP_CMD} | wc -l ) -ge \${MAX_REMOTE_BACKUPS} ];
+    do
+        BACKUP_TO_BE_DELETED=\$(echo ls -1 /mongobackup/${BACKUP_REMOTE_SUBFOLDER} | ${SFTP_CMD} | sort | head -n 1)
+        echo "   Deleting REMOTE backup \${BACKUP_TO_BE_DELETED}"
+        DBFOLDER_TO_BE_DELETED=\$(echo ls -1 \${BACKUP_TO_BE_DELETED} | ${SFTP_CMD} | sort | head -n 1)
+        echo rm \${DBFOLDER_TO_BE_DELETED}/* | ${SFTP_CMD}
+        echo rmdir \${DBFOLDER_TO_BE_DELETED} | ${SFTP_CMD}
+        echo rmdir \${BACKUP_TO_BE_DELETED} | ${SFTP_CMD}
+    done
+fi
+
+
+
 echo "=> Backup done"
 EOF
 chmod +x /backup.sh
@@ -58,6 +81,10 @@ chmod +x /restore.sh
 
 touch /mongo_backup.log
 tail -F /mongo_backup.log &
+
+#echo "=> Mount sshfs"
+#umount /backup
+#echo ${BACKUP_SERVER_PASS} | sshfs ${BACKUP_SERVER_USER}@${BACKUP_SERVER}:/mongo/${MONGODB_USER} /backup -o StrictHostKeyChecking=no -o password_stdin && echo "  Mount successful" || echo "  Mount failed"
 
 if [ -n "${INIT_BACKUP}" ]; then
     echo "=> Create a backup on the startup"
